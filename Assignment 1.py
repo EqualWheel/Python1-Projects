39de3f5ecbf6876b58150b9fe002733c5391e5aa
EqualWheel <111201182+EqualWheel@users.noreply.github.com>
2024-09-26 14:43:25 -0400

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,22 @@
+
+
+
+# Challenge: Handle cases where the user enters non-numeric input for the principal amount, interest rate, or time period, and provide appropriate error messages.
+# Input: Prompt the user to enter the principal amount, interest rate(in percentage) and the time period(in years).
+# Processing: Calculate the simple interest using the formula: Simple interest: (Principal * Rate * Time)/ 100.
+# Output: Display the calculated simple interest.
+
+
+
+# Input for 3 different variables
+
+principle = float(input("Enter the principal amount: "))
+rate = float(input("Enter the interest rate: "))
+time = float(input("Enter the time period in years: "))
+
+# Processing the simple interest
+
+simple_interest = (principle * rate * time) / 100
+
+# This is the output for simple interest
+print("Simple Interest is: ", simple_interest)
---
+++ Assignment 17.py
@@ -0,0 +1,34 @@
+
+
+
+# Challenge: Optimize the function to handle large input list efficiently.
+# Description: Develop a function called find_common_elements that takes two list as input and returns a new list containing elements that are in common to both input list.
+
+
+
+
+def find_common_elements(list1, list2):
+    # Convert list1 to a set for efficient lookups
+    set1 = set(list1)
+
+    # Initialize an empty list to store common elements
+    common = []
+
+    # Loop through elements in list2 and check for common elements
+    for element in list2:
+        if element in set1:
+            common.append(element)
+
+    # Return the list of common elements
+    return common
+
+# Input: Prompt the user to user to enter two lists
+list1 = input("Enter the first list of elements separated by spaces: ").split()
+list2 = input("Enter the second list of elements separated by spaces: ").split()
+
+# Processing and Output: Find and display common elements
+common_elements = find_common_elements(list1, list2)
+if common_elements:
+    print(f"Common elements: {', '.join(common_elements)}")
+else:
+    print("No common elements found.")
